f5e821139b1a085bb3af23c6f0ebd95eed9a84a2
tuanteng96 <30353855+tuanteng96@users.noreply.github.com>
2022-05-05 16:17:04 +0700

    update

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,3 +1,3 @@
 export const SERVER_APP = "https://hanababyspa.com";
-export const NAME_APP = "HANA BABY SPA";
-export const VERSION_APP = "1.0.0";
+export const NAME_APP = "Hana Baby";
+export const VERSION_APP = "2.0.0";
